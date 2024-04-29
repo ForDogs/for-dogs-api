6c76504ba6eb69cccbc9a7200844e6f52ad73dfc
Leo <drd9811@naver.com>
2024-04-29 15:50:01 +0900

    REFACTOR: 패키지 구조 변경

--- src/main/java/com/fordogs/ForDogsApiApplication.java
+++ src/main/java/com/fordogs/ForDogsApiApplication.java
@@ -1,7 +1,6 @@
-package com.fordogs.fordogsapi;
+package com.fordogs;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
