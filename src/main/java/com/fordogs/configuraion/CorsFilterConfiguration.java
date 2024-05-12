ebe6c55ac82b7524e21b7f4e18c1d2e708d1a2d0
Leo <drd9811@naver.com>
2024-05-12 14:18:36 +0900

    FEAT: CORS 설정 적용

---
+++ src/main/java/com/fordogs/configuraion/CorsFilterConfiguration.java
@@ -0,0 +1,26 @@
+package com.fordogs.configuraion;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.List;
+
+@Configuration
+public class CorsFilterConfiguration {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("*"); // TODO: 2024-05-12 배포 후 URI 변경할 것
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
+        config.addAllowedHeader("*");
+        config.setAllowCredentials(true);
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsFilter(source);
+    }
+}
