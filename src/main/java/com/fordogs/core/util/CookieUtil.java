d50c5449c759fe418f83cfd32e8359792d3f60f7
Leo <drd9811@naver.com>
2024-06-16 19:05:44 +0900

    FIX: 리프레쉬 토큰 쿠키 도메인 추가

--- src/main/java/com/fordogs/core/util/CookieUtil.java
+++ src/main/java/com/fordogs/core/util/CookieUtil.java
@@ -12,15 +12,18 @@ import java.time.LocalDateTime;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class CookieUtil {
 
+    public static final String SERVICE_DOMAIN = "fordogs.store";
+
     public static String createRefreshTokenCookie(RefreshToken refreshToken) {
         long maxAge = (refreshToken.getExpiration() != null) ?
                 Duration.between(LocalDateTime.now(), refreshToken.getExpiration()).getSeconds() : -1;
 
         return ResponseCookie.from(HttpResponseConstants.COOKIE_NAME_REFRESH_TOKEN, refreshToken.getValue())
                 .path("/")
-                .sameSite("None")
+                .domain(SERVICE_DOMAIN)
                 .httpOnly(true)
                 .secure(true)
+                .sameSite("Lax")
                 .maxAge(maxAge)
                 .build()
                 .toString();
