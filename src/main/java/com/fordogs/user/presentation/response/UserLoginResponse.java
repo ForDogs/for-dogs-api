a73b9a7f77fedd8ad933356d5cfedc7348d48895
Leo <drd9811@naver.com>
2024-09-27 16:27:54 +0900

    FIX: 로그인 응답 공급자 데이터 추가

--- src/main/java/com/fordogs/user/presentation/response/UserLoginResponse.java
+++ src/main/java/com/fordogs/user/presentation/response/UserLoginResponse.java
@@ -16,12 +16,16 @@ public class UserLoginResponse {
     @Schema(description = "로그인 회원 ID")
     private String userId;
 
+    @Schema(description = "로그인 공급자")
+    private String provider;
+
     @Schema(description = "Access Token")
     private TokenInfo accessToken;
 
     public static UserLoginResponse toResponse(UserEntity userEntity, AccessToken accessToken) {
         return UserLoginResponse.builder()
                 .userId(userEntity.getAccount().getValue())
+                .provider(userEntity.getProvider().name())
                 .accessToken(TokenInfo.toResponse(accessToken.getValue(), accessToken.getMetadata().getExpirationTime()))
                 .build();
     }
