a4dd06d0d4c964d856077d172406c4c35aa3acc4
Leo <drd9811@naver.com>
2024-10-22 18:07:19 +0900

    FIX: DynamoDB 과금으로 인한 에러 로깅 중지

--- src/main/java/com/fordogs/core/util/logging/ApiLoggingAspect.java
+++ src/main/java/com/fordogs/core/util/logging/ApiLoggingAspect.java
@@ -3,9 +3,7 @@ package com.fordogs.core.util.logging;
 import com.fordogs.core.presentation.ErrorResponse;
 import lombok.RequiredArgsConstructor;
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
@@ -17,7 +15,8 @@ public class ApiLoggingAspect extends ApiLoggingPointCut {
 
     private final ApiLogger apiLogger;
 
-    @Before(value = "apiLoggingPointcut()")
+    // TODO: DynamoDB 과금으로 인한 에러 로깅 중지
+    // @Before(value = "apiLoggingPointcut()")
     public void apiBeforeLogging(JoinPoint joinPoint) {
         Class<?> returnType = ((MethodSignature) joinPoint.getSignature()).getReturnType();
         if (ResponseEntity.class.isAssignableFrom(returnType)) {
@@ -25,12 +24,13 @@ public class ApiLoggingAspect extends ApiLoggingPointCut {
         }
     }
 
-    @AfterReturning(value = "apiLoggingPointcut()", returning = "response")
+    // TODO: DynamoDB 과금으로 인한 에러 로깅 중지
+    // @AfterReturning(value = "apiLoggingPointcut()", returning = "response")
     public void apiAfterLogging(ResponseEntity<?> response) {
         apiLogger.afterLog(response);
     }
 
-    // TODO: 2024.08.30 DynamoDB 과금으로 인한 에러 로깅 중지
+    // TODO: DynamoDB 과금으로 인한 에러 로깅 중지
     // @AfterReturning(value = "within(com.fordogs.core.exception.GlobalExceptionHandler)", returning = "response")
     public void apiThrowException(ResponseEntity<ErrorResponse> response) {
         apiLogger.failLog(response);
